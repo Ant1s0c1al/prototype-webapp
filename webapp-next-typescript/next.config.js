95c607d9342926d3b917c37f979e88bac0255ee7
Ant1s0c1al <ant1s0c1al.opensource@gmail.com>
2022-11-24 21:39:58 +0000

    config: ExperimentalFeatures Available

--- webapp-next-typescript/next.config.js
+++ webapp-next-typescript/next.config.js
@@ -7,6 +7,13 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  /*
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ['prisma'],
+    transpilePackages: ['@acme/ui', 'lodash-es'],
+  },
+  */
 }
 
 module.exports = nextConfig;
